0a7d93ce08f59462a1e20e4367fb118fbd0b5f57
Chen Zhu <czhu2@washcoll.edu>
2018-01-28 13:39:58 -0500

    Update Week_1.py

--- Week_1.py
+++ Week_1.py
@@ -6,5 +6,5 @@ print ("The list of numbers are: ", numbers)
 avg = [sum(numbers) / len(numbers)] #avg of total numbers from user input
 print ("The average of the numbers is: ", avg)
 
-n = [(a + b) / 2 for a, b in zip(numbers[::2], numbers[1::2])] #avg of two consecutive numbers
+avgTwo = [(a + b) / 2 for a, b in zip(numbers[::2], numbers[1::2])] #avg of two consecutive numbers
 print ("The average of two consecutive numbers are: ", n)
