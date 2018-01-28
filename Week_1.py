e328b1e8d27ac5eac61ed640508b54adf520e10d
Chen Zhu <czhu2@washcoll.edu>
2018-01-28 13:39:22 -0500

    Update Week_1.py

--- Week_1.py
+++ Week_1.py
@@ -1,9 +1,9 @@
 user = "Hank"
 
-numbers = [int(n) for n in input("Enter numbers: ").split()]
+numbers = [int(i) for i in input("Enter numbers: ").split()] #ask user to give list of numbers
 print ("The list of numbers are: ", numbers)
 
-avg = [sum(numbers) / len(numbers)]
+avg = [sum(numbers) / len(numbers)] #avg of total numbers from user input
 print ("The average of the numbers is: ", avg)
 
 n = [(a + b) / 2 for a, b in zip(numbers[::2], numbers[1::2])] #avg of two consecutive numbers
