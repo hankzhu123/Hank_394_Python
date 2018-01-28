14a89521d78caec0316625709273d7462b108090
Chen Zhu <czhu2@washcoll.edu>
2018-01-28 13:36:13 -0500

    Update and rename HelloWorld.txt to HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,10 @@
+user = "Hank"
+
+numbers = [int(n) for n in input("Enter numbers: ").split()]
+print ("The list of numbers is: ", numbers)
+
+avg = [sum(numbers) / len(numbers)]
+print ("The average of the numbers is: ", avg)
+
+n = [(a + b) / 2 for a, b in zip(numbers[::2], numbers[1::2])] #avg of two consecutive numbers
+print ("The average of two consecutive numbers is: ", n)
